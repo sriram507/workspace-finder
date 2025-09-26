1f8d5a60db1bca62d859b6addf8c8e01acf296df
sriram507 <isreeram2014@gmail.com>
2025-09-26 11:40:30 +0530

    Fix routing with hashrouter for github pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { useState } from "react";
+// import Navbar from "./components/Navbar";
+// import SearchBar from "./components/SearchBar";
+// import WorkspaceList from "./components/WorkspaceList";
+// import WorkspaceDetails from "./components/WorkspaceDetails";
+
+// function App() {
+//   const [query, setQuery] = useState("");
+
+//   return (
+//     <Router>
+//       <Navbar />
+//       <SearchBar query={query} setQuery={setQuery} />
+//       <Routes>
+//         <Route path="/" element={<WorkspaceList query={query} />} />
+//         <Route path="/workspace/:id" element={<WorkspaceDetails />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import SearchBar from "./components/SearchBar";
@@ -11,9 +37,16 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <SearchBar query={query} setQuery={setQuery} />
       <Routes>
-        <Route path="/" element={<WorkspaceList query={query} />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <SearchBar query={query} setQuery={setQuery} />
+              <WorkspaceList query={query} />
+            </>
+          }
+        />
         <Route path="/workspace/:id" element={<WorkspaceDetails />} />
       </Routes>
     </Router>
